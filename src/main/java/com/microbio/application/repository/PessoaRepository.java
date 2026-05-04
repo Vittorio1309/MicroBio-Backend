0485f77b2dbea169a0e540516e546a2c4ff262e7
Vittorio Uecker <vittoriouecker@gmail.com>
2026-05-04 20:33:14 -0300

    feat: atualizar PessoaRepository com busca por email

--- src/main/java/com/microbio/application/repository/PessoaRepository.java
+++ src/main/java/com/microbio/application/repository/PessoaRepository.java
@@ -4,6 +4,10 @@ import com.microbio.application.model.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+    Optional<Pessoa> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
