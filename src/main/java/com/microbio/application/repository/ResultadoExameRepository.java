2b578a1efe216f15c56db127329a4b65720906d3
Vittorio Uecker <vittoriouecker@gmail.com>
2026-05-04 20:33:13 -0300

    feat: adicionar ResultadoExameRepository

---
+++ src/main/java/com/microbio/application/repository/ResultadoExameRepository.java
@@ -0,0 +1,13 @@
+package com.microbio.application.repository;
+
+import com.microbio.application.model.ResultadoExame;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ResultadoExameRepository extends JpaRepository<ResultadoExame, Long> {
+    List<ResultadoExame> findByOrcamentoId(Long orcamentoId);
+    List<ResultadoExame> findByOrcamentoPessoaId(Long pessoaId);
+}
