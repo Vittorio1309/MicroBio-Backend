246d7f60d7ed508b260a553a72d35e163756b4a5
Vittorio Uecker <vittoriouecker@gmail.com>
2026-05-04 20:33:13 -0300

    feat: atualizar OrcamentoRepository com queries por pessoa e status

--- src/main/java/com/microbio/application/repository/OrcamentoRepository.java
+++ src/main/java/com/microbio/application/repository/OrcamentoRepository.java
@@ -1,9 +1,15 @@
 package com.microbio.application.repository;
 
 import com.microbio.application.model.Orcamento;
+import com.microbio.application.model.OrcamentoStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrcamentoRepository extends JpaRepository<Orcamento, Long> {
+    List<Orcamento> findByPessoaId(Long pessoaId);
+    List<Orcamento> findByServicoId(Long servicoId);
+    List<Orcamento> findByStatus(OrcamentoStatus status);
 }
