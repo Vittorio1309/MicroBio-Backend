7e16b1716b1b255018ceb9a1beff64c20a2d7d8c
Vittorio Uecker <vittoriouecker@gmail.com>
2026-05-04 20:31:44 -0300

    feat: adicionar exceção BusinessException

---
+++ src/main/java/com/microbio/application/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.microbio.application.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+}
