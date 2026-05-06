add1b77816a355eab67be9830e097a2ea7774a57
Vittorio Uecker <vittoriouecker@gmail.com>
2026-05-05 21:50:06 -0300

    feat: correção do restultado exame repository

--- src/main/java/com/microbio/application/repository/ResultadoExameRepository.java
+++ src/main/java/com/microbio/application/repository/ResultadoExameRepository.java
@@ -2,7 +2,6 @@ package com.microbio.application.repository;
 
 import com.microbio.application.model.ResultadoExame;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,7 +10,5 @@ import java.util.List;
 public interface ResultadoExameRepository extends JpaRepository<ResultadoExame, Long> {
     List<ResultadoExame> findByOrcamentoId(Long orcamentoId);
     List<ResultadoExame> findByOrcamentoPessoaId(Long pessoaId);
-    
-    @Query("SELECT r.orcamento.id FROM ResultadoExame r WHERE r.orcamento.id = ?1 LIMIT 1")
     boolean existsByOrcamentoId(Long orcamentoId);
 }
